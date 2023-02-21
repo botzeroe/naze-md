5bab981bd328387516d287d12fe6595dc288679d
botzeroe <124211008+botzeroe@users.noreply.github.com>
2023-02-21 23:11:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
+global.owner = ['967776008370']
 global.ownernomer = "6285875158363"
 global.premium = ['6285875158363']
 global.packname = 'Sticker By'
